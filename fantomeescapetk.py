04849aa3111fbbb6c0fcdc7bc2ad8463a75c63d2
MiharuFarron <ayeshafarron@gmail.com>
2018-11-28 16:30:30 +0100

    Maj double

--- fantomeescapetk.py
+++ fantomeescapetk.py
@@ -94,10 +94,10 @@ def droite(i,j,prev,grille):
         position(i,j+1,grille)
         Trigger(Gasper,Liste_monstre,Pintes)
         if mv==1:
-            Retour_recep()
+            Retour_recep(x,y,value)
             grille[i][j+1]=value
         elif mv==0 :
-            Fou_depl(Index_room)
+            Fou_depl(x,y,value,Index_room)
             grille[i][j+1]=value
             Trigger(Gasper,Liste_monstre,Pintes)
         else :
@@ -119,10 +119,10 @@ def gauche(i,j,prev,grille):
         position(i,j-1,grille)
         Trigger(Gasper,Liste_monstre,Pintes)
         if mv==1:
-            Retour_recep()
+            Retour_recep(x,y,value)
             grille[i][j-1]=value
         elif mv==0 :
-            Fou_depl(Index_room)
+            Fou_depl(x,y,value,Index_room)
             grille[i][j-1]=value
             Trigger(Gasper,Liste_monstre,Pintes)
         else :
@@ -145,10 +145,10 @@ def haut(i,j,prev,grille):
         position(i-1,j,grille)
         Trigger(Gasper,Liste_monstre,Pintes)
         if mv==1:
-            Retour_recep()
+            Retour_recep(x,y,value)
             grille[i-1][j]=value
         elif mv==0 :
-            Fou_depl(Index_room)
+            Fou_depl(x,y,value,Index_room)
             grille[i-1][j]=value
             Trigger(Gasper,Liste_monstre,Pintes)
         else :
@@ -170,10 +170,10 @@ def bas(i,j,prev,grille):
         position(i+1,j,grille)
         Trigger(Gasper,Liste_monstre,Pintes)
         if mv==1:
-            Retour_recep()
+            Retour_recep(x,y,value)
             grille[i+1][j]=value
         elif mv==0 :
-            Fou_depl(Index_room)
+            Fou_depl(x,y,value,Index_room)
             grille[i+1][j]=value
             Trigger(Gasper,Liste_monstre,Pintes)
         else :
@@ -248,12 +248,13 @@ def Is_one_case_range(Monstre,joueur):
 
 #Maitre du chateau
 
-def Retour_recep () :
+def Retour_recep (i,j,prev) :
     """modifie les coordonnées de joueur ce qui le place à la réception"""
     global x
     global y
     global value
     showinfo("Cher visiteur","Laissez-moi vous raccompagner à l'entrée de ma demeure")
+    grille[i][j]=prev
     position(8,4,Plateau)
     x=8
     y=4
@@ -265,7 +266,7 @@ def Fou_take_pinte(joueur):
     joueur["pinte"] = joueur["pinte"] - 1 #modifie la valeur dans le dict Gasper
     showinfo("Comment oses-tu rentrer dans mon laboratoire ?","Gasper perd une pinte d'energie, il lui reste %d pinte(s) d'energie"%(Gasper['pinte']))
 
-def Fou_depl(listR):
+def Fou_depl(i,j,prev,listR):
     """pouvoir du Fou qui modifie de manière aléatoire la position de Gasper"""
     global x
     global y
@@ -273,6 +274,7 @@ def Fou_depl(listR):
     r = random.randint(0,11)
     x = listR[r][0]
     y = listR[r][1]
+    grille[i][j]=prev
     position(x,y,Plateau)
 
 #Chamallow bibbendum:
